a8f1c9b8c89f4a186dcf22872949d04cc990fb66
syahputrawork98-sketch <syah.putrawork98@gmail.com>
2025-09-16 12:57:07 +0700

    berhasil install font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-
+// App.jsx
 import React from 'react'
 import './App.css'
 
 
 const App = () => {
   return (
-    <div>App</div>
+   
+    <div className="p-8">
+      <h1 className="text-4xl font-cormorant font-bold">
+        Ini Judul dengan Cormorant Garamond
+      </h1>
+      <p className="mt-4 font-montserrat ">
+        Ini paragraf dengan Montserrat.
+      </p>
+    </div>
+
+
+
   )
 }
 
